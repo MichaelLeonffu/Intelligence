8d0d419a24380f67af8186de528cfc7619bb6ca8
Michael Leonffu <leonffu.michael@gmail.com>
2018-03-25 14:22:46 -0700

    Upgraded point; can now find distances between points

--- snakeGame/Point.java
+++ snakeGame/Point.java
@@ -11,6 +11,7 @@ public class Point{
 	private int x;
 	private int y;
 
+	//Constructor
 	public Point(){
 		this(0, 0);
 	}
@@ -22,6 +23,31 @@ public class Point{
 	public Point(int x, int y){
 		this.x = x;
 		this.y = y;
+	}
+
+	//Methods
+	public double distance(Point point){	//should it check for null?!
+		return this.distance(this, point);
+	}
+
+	public double distance(Point point1, Point point2){
+		return this.distance(point1.x, point1.y, point2.x, point2.y);
+	}
+
+	public static double distance(int x1, int y1, int x2, int y2){
+		return Math.pow(((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)), 0.5);
+	}
+
+	public int distanceInt(Point point){
+		return this.distanceInt(this, point);
+	}
+
+	public int distanceInt(Point point1, Point point2){
+		return this.distanceInt(point1.x, point1.y, point2.x, point2.y);
+	}
+
+	public static int distanceInt(int x1, int y1, int x2, int y2){
+		return Math.abs(x1 - x2) + Math.abs(y1 - y2);
 	}
 
 	//Accessor
@@ -48,6 +74,7 @@ public class Point{
 		return true;
 	}
 
+	//Misc
 	public boolean equals(Object pointObject){
 		if(pointObject == null)						//If null then its false
 			return false;
