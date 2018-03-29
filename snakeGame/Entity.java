353a53428c0b6d94b762243091032370a718f7c9
Michael Leonffu <leonffu.michael@gmail.com>
2018-03-28 21:51:03 -0700

    Updateing prints, moving methods in order

--- snakeGame/Entity.java
+++ snakeGame/Entity.java
@@ -101,25 +101,26 @@ public abstract class Entity{
 	}
 
 	//Behavoirs
-	public abstract boolean spawn(Game field);		//Alows entites to spawn other entities
-	public abstract boolean relocate(Game field); 	//For relocating positions via method; finding and moving to valid location
-	public abstract boolean upkeep(Game field);		//Game phase to check current entity situation and update
-	public abstract boolean action(Game field);		//Game phase where each entity makes a move in the game space
+	public abstract boolean spawn(Game game);		//Alows entites to spawn other entities
+	public abstract boolean relocate(Game game); 	//For relocating positions via method; finding and moving to valid location
+	public abstract boolean upkeep(Game game);		//Game phase to check current entity situation and update
+	public abstract boolean action(Game game);		//Game phase where each entity makes a move in the game space
 
 	public String toString(){
-		return this.name + ": " + this.symbol + ": " + this.point + " Animate: " + this.animate + " Prec: " + this.precedence + " view: " + this.view + " fit: " + this.fitness + " exist: " + this.exist;
+		return this.name + ": " + this.symbol + ": " + this.point + " Animate: " + this.animate + " Prec: " + this.precedence + " Mortal: " + this.mortal + " view: " + this.view + " fit: " + this.fitness + " exist: " + this.exist;
 	}
 
+	@Deprecated
 	public void print(){
 		System.out.printf("name: %10s symbol: %1s point: (%3d,%3d) animate: %5s precedence: %3d view: %3d fitness: %5d exist: %5s%n", this.name, this.symbol, this.point.getX(), this.point.getY(), this.animate, this.precedence, this.view, this.fitness, this.exist);
 	}
 
 	public void printHeader(){
-		System.out.printf("%-15s %-6s %-9s %-7s %-10s %-4s %-7s %-5s%n", "name", "symbol", "point", "animate", "precedence", "view", "fitness", "exist");
+		System.out.printf("%-15s %-6s %-9s %-7s %-10s %-6s %-4s %-7s %-5s%n", "name", "symbol", "point", "animate", "precedence", "mortal", "view", "fitness", "exist");
 	}
 
 	public void printBody(){
-		System.out.printf("%-15s %-6s (%3d,%3d) %-7s %10d %4d %7d %-5s%n", this.name, this.symbol, this.point.getX(), this.point.getY(), this.animate, this.precedence, this.view, this.fitness, this.exist);
+		System.out.printf("%-15s %-6s (%3d,%3d) %-7s %10d %-6s %4d %7d %-5s%n", this.name, this.symbol, this.point.getX(), this.point.getY(), this.animate, this.precedence, this.mortal, this.view, this.fitness, this.exist);
 	}
 
 }
