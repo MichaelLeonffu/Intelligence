97199248d8621c27a32ce9e385a24ec6e23ec6a8
Michael Leonffu <leonffu.michael@gmail.com>
2018-03-29 18:37:02 -0700

    Updated version

--- algorithms/Sigmoid.java
+++ algorithms/Sigmoid.java
@@ -3,8 +3,8 @@
 *	Sigmoid. https://en.wikipedia.org/wiki/Sigmoid_function
 *	
 *	@author Michael Leonffu
-*	@version v0.3.1-alpha
-*	@since v0.3.1-alpha
+*	@version v0.3.2-alpha
+*	@since v0.3.2-alpha
 *
 */
 package intelligence.algorithms;
